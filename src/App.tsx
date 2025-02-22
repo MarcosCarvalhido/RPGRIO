702686708a58cf10bd1aa60271e386a406a5b009
Marcos Carvalhido Rodrigues <marcoscrodrigues8@gmail.com>
2025-02-22 19:54:59 -0300

    Joça continuada

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
+import Event from "./Pages/EventContent"
 
 import NavBar from "./Components/NavBar";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [nome,setEvento] = useState("Tagmar")
   return (
     <BrowserRouter>
       <NavBar />
@@ -19,6 +23,9 @@ function App() {
       </Route>
       <Route path="/sobre">
         <About />
+      </Route>
+      <Route path="/evento">
+        <Event nome={"Tagmar"} />
       </Route>
     </BrowserRouter>
   );
---
+++ src/Pages/EventContent.tsx
@@ -0,0 +1,12 @@
+function EventContent({nome}: {nome: string}){
+
+    return(
+        <div>
+            <h1>
+                CONTEUDO {nome}
+            </h1>
+        </div>
+    )
+}
+
+export default EventContent
